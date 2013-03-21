0400b0412135bbe15dcb43309f897fd58d200759
AJ Norton <ajn123@vt.edy>
2013-03-21 02:27:04 -0400

    added struct example

--- hello.c
+++ hello.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct {
+//@author AJ Norton
 
-	int i;
-	struct food *point;
+typedef struct {  // initializes a structure
+	//typef is used to avoid writing "struct" and just have to write the type "food"
+
+	int calories;// variable contained in each struct.
+	int fat;
+
+	struct food *pointer;  // a structure can contain a pointer to the same structure type or any other type.
 } food;
 
-int factorial(int i);
 
 int main() {
 
-	for(int i = 0; i <10;i++)
-	{
 
-		printf("%d \n", i );
-	}
+
+
+    food sandwich = {500,12}; //Initializes calories to 500. and fat to 12
+
+    food burger = {400}; //Initializes calories to 400 and fat to 0.
+    //the initialize format works by assigning values to the variables from top to bottom in the struct
+    //so the order of the struct variables in the definition impacts how they are defined.
+    //in this case calories is defined first because it is the first thing defined in the struct, fat is second and so on
+
+    burger.calories = 100; //changes the number of calories in the structure by using Dot notation.
+    //dot notation can be used to access the variable and modify it.
+
+
+    food *pointerToFood = &sandwich; // declares a pointer of type food and assigns it the address of sandwich
+
+
+
+    pointerToFood->pointer = &burger;// uses  arrow notation which is how you assign values to POINTERS
+
+
+    //This will loop through the pointer until it pointing to NUll, it keeps assigning the pointer to the pointer
+    //of the struct is currently pointing to, this provides a way to link structure and loop through them.
+
+    for(;pointerToFood != NULL; pointerToFood = pointerToFood->pointer)
+    {
+    	printf("%d \n",pointerToFood->calories );
+    }
+
+
+    /**
+     * output:
+     * 500
+     * 100
+     */
 
 
 
@@ -24,11 +58,3 @@ int main() {
 	return 0;
 }
 
-int factorial(int i) {
-	if (i == 0) {
-		return 1;
-	} else {
-		return i * factorial(i - 1);
-	}
-
-}
