f189d380612202243f1723e0da710c2d7858854a
AJ Norton <ajn123@vt.edy>
2013-03-21 10:02:36 -0400

    recursion example

---
+++ Recursion.c
@@ -0,0 +1,36 @@
+/*
+ * Recursion.c
+ *
+ *  Created on: Mar 21, 2013
+ *      Author: Aj Norton
+ */
+#include <stdio.h>
+
+int factorial(int i);// method header so the the compiler knows the function is defined below main
+
+int main(void)
+{
+  printf("%d", factorial(5));
+
+
+	return 0;
+}
+
+
+int factorial(int i)
+{
+	if(i <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return factorial(i-1)*i; //returns factorial times the inputed value, effectively multiplying n*(n-1)*(n-2)*(n-3) = n!
+	}
+}
+
+
+
+
+
+
