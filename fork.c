88e1d76e4164b1a22d5e4bc58b8c9ecef4372f9a
AJ Norton <aj123@vt.edu>
2014-01-23 19:59:51 -0500

    fork added

---
+++ fork.c
@@ -0,0 +1,44 @@
+/*
+ * fork.c
+ *
+ *  Created on: Jan 23, 2014
+ *      Author: marknorton
+ */
+
+/*
+ * Const.c
+ *
+ *  Created on: Mar 25, 2013
+ *      Author: Aj Norton
+ */
+#include <stdio.h>
+
+//constant
+
+int main(void) {
+
+	/**
+	 * the fork function runs the same program in a new child process that is a
+	 * duplicate of the parent.
+	 */
+
+	printf("hello  \n");
+	int a = fork();
+
+	int x = 1;
+
+	if (a == 0) {
+		printf("x is %d \n", ++x);
+		exit(0);
+	}
+
+	printf("other x is %d \n", --x);
+
+	/**
+	 other x is 0
+	 x is 2
+	 */
+
+	return 0;
+}
+
