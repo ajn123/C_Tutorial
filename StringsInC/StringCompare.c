cc2bae54eccb556416944902a160a4a5df2ab886
AJ Norton <aj123@vt.edu>
2014-04-08 09:52:22 -0400

    added readme to pointers folder

--- StringsInC/StringCompare.c
+++ StringsInC/StringCompare.c
@@ -3,6 +3,11 @@
  *
  *  Created on: Apr 29, 2013
  *      Author: Aj Norton
+ *
+ *
+ *      This file shows an example of how to write a string compare method,
+ *      how to iterate through a string and compare them.
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,10 +17,8 @@
 
 bool compare(char * string1, char * string2);
 
-
-int main(void) {
-
-
+int main(void)
+{
 	char * string1 = NULL;
 	char * string2 = "ajnjjj";
 
@@ -29,11 +32,11 @@ bool compare(char *string1,char *string2)
 	if(string1 == NULL || string2 == NULL) return false;
 
 
-   if(strlen(string1) != strlen(string2))  return false;
+	if(strlen(string1) != strlen(string2))  return false;
 
-   while( *string1 != '\0' || *string2 != '\0')
+   while( *string1 != '\0' || *string2 != '\0')//Loops through strings until null terminator is found.
    {
-       if(*string1 != *string2)
+       if(*string1 != *string2)//if the chars are different, the strings are not the same, return false.
        {
     	   return false;
        }
@@ -42,14 +45,6 @@ bool compare(char *string1,char *string2)
    }
 
    return true;
-
-
-
-
-
-
-
-
 }
 
 
